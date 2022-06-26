79f546c5c4213b1234492702210ce28b0f3de9b8
Han2iN0 <105941134+Han2iN0@users.noreply.github.com>
2022-06-26 19:34:19 +0100

    Copying main code into minMax

--- minMax.js
+++ minMax.js
@@ -1,1 +1,20 @@
-hffh
+const minMax = () => {
+    let arr = prompt ('Please State the list of numbers you would like to be invistigated').string(',')
+    let min = arr[0]
+    let max = arr[0]
+    let returnArr = []
+    
+    for (let i=0 ; arr.length>=i ; i++) {
+        if (min<arr[i]) {
+            min = arr[i]
+        }
+        if (max > arr[i]) {
+            max = arr[i]
+        }
+    }
+    returnArr.push(max)
+    returnArr.push(min)
+    return returnArr
+}
+
+console.log(minMax())
