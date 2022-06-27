ed38629a753ca382b4c36f8eefa20c33293f26a3
Han2iN0 <105941134+Han2iN0@users.noreply.github.com>
2022-06-28 00:40:26 +0100

    Final Update (operational)

--- age.js
+++ age.js
@@ -1,14 +1,17 @@
-const birthDate = () => {
-  const userBirthday = prompt('What is your birthday (mm/dd/yyyy)')
-  const dob = new Date(userBirthday)
-  
-  const month_diff = Date.now() - dob.getTime()
-  const age_dt = new Date(month_diff); 
- var year = age_dt.getUTCFullYear();
-    var age = Math.abs(year - 1970);
-     //display the calculated age
-    console.log("Age of the date entered: " + age + " years");
+const today  = new Date 
+const [month,year,day] = [today.getMonth(),today.getFullYear(),today.getDay()]
+
+const ageCalc = () => {
+    const userInput = prompt ('please enter your date of birth (dd/mm/yyyy)
+const birthDay = userInput.substring(0,2)
+const birthMonth = userInput.substring(3,5)
+const birthYear = userInput.substring(6)
+
+let age = year - birthYear
+    if (birthMonth>month || birthMonth === month && birthDay<day) {
+        age -=1
+    }
+    return `Your age is: ${age}`
 }
 
-birthDate()
-
+console.log(ageCalc())
